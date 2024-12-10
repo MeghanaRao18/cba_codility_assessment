0392f3b6db42d08159eb09a5171cfd3774f144ef
Meghana Rao <Meghana.Rao@endava.com>
2024-12-10 15:21:24 +1100

    fixed pipeline worklow issue with path

--- tests/test_pet_endpoints.py
+++ tests/test_pet_endpoints.py
@@ -334,6 +334,7 @@ def test_upload_pet_image(add_data,config_manager,generate_data):
     current_dir = os.path.dirname(__file__)
     #image_path = os.path.abspath(os.path.join(current_dir, "..", "config", "img1.jpg"))
     image_path = os.path.join(os.path.dirname(__file__),"..", "config", "img1.jpg")
+    print(f'image path {image_path}')
     assert os.path.exists(image_path)
     add_data.photo =image_path
     config_manager.set_http_request_body_with_pet_photo(add_data)
