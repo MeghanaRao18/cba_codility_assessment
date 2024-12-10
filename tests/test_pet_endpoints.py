5d49dbda93686e7ce9f98e2318b17f088615984d
Meghana Rao <Meghana.Rao@endava.com>
2024-12-10 15:34:54 +1100

    added images folder

--- tests/test_pet_endpoints.py
+++ tests/test_pet_endpoints.py
@@ -342,7 +342,7 @@ def test_upload_pet_image(add_data,config_manager,generate_data):
     url_temp = config_manager.get_basic_url()
     url_temp += config_manager.get_endpoint()
     config_manager.set_post_uploadimage_response_full(url_temp)
-    print (f'response {config_manager.get_response_full_json()}')
+    print (f'response {config_manager.get_response_full()}')
     # add response code
     expected_code = 200
 
